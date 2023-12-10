c3a0da7f7b2ae0c655b86bbf81ebbb4d551bb4cb
ZhengNan6 <119780222+ZhengNan6@users.noreply.github.com>
2023-12-10 18:01:31 +0800

    Delete COMMUNICATION directory

--- COMMUNICATION/can2_send.h
+++
@@ -1,9 +0,0 @@
-#ifndef __CAN2_SEND_H
-#define __CAN2_SEND_H
-
-#include "gimbal_struct_variables.h"
-
-//extern void can2_gimbal_to_chassis(void);
-extern void can2_gimbal_setmsg_to_yaw(int16_t yaw);
-extern void can2_gimbal_to_chassis(void);
-#endif
